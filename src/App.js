a0136d4acea7dc3993e2396bee9b0d64015c233d
Alex Nordstrom <6267244+HyperEpsilon@users.noreply.github.com>
2019-01-19 19:13:39 -0700

    Added alias for firebase deploy

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            I'm sorry Sam, I love you
           </a>
         </header>
       </div>
