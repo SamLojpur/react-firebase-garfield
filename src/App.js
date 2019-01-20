38052799ea9aff7556ef32e17b13c0b369863442
Alex Nordstrom <6267244+HyperEpsilon@users.noreply.github.com>
2019-01-20 03:15:43 -0700

    Made my own version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ImageContainer from './components/ImageContainer';
+import './App.css';
 
 export default class Page extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            data: null	
+			URL: null,
+			id: null	
         }
     }
 
@@ -26,7 +28,7 @@ export default class Page extends React.Component {
         // getMyDatabaseStuff(query)
         // .then( (result) => {
 
-            this.setState({data:{
+            this.setState({URL:{
                 A1: "https://firebasestorage.googleapis.com/v0/b/searchforthebestgarfieldcomic.appspot.com/o/panels%2F1985-01-01a.gif?alt=media&token=8fae9162-f80c-45c1-b151-5a7c4804f7bc",
                 B1: "https://firebasestorage.googleapis.com/v0/b/searchforthebestgarfieldcomic.appspot.com/o/panels%2F1985-01-01b.gif?alt=media&token=7bef96aa-7418-4d87-9ce4-d7fbbbd035a0",
                 C1: "https://firebasestorage.googleapis.com/v0/b/searchforthebestgarfieldcomic.appspot.com/o/panels%2F1985-01-01c.gif?alt=media&token=c53f2fed-101d-4847-95c8-6b442798906e",
@@ -39,26 +41,28 @@ export default class Page extends React.Component {
 
 
     render() {
-        if(!this.state.data) {
+        if(!this.state.URL) {
             return null;
         } else {
             return (
-                <div>
-                    <div onClick={() => {this.handleVotingStuff(1)}}>
-						<ImageContainer>
-							<img src={this.state.data.A1}></img>
-							<img src={this.state.data.B1}></img>
-							<img src={this.state.data.C1}></img>
-						</ImageContainer>
-					</div>
-                    Which is funnier?
-					<div onClick={() => {this.handleVotingStuff(2)}}>
-						<ImageContainer>
-							<img src={this.state.data.A2}></img>
-							<img src={this.state.data.B2}></img>
-							<img src={this.state.data.C2}></img>
-						</ImageContainer>
-					</div>
+                <div className="App">
+					<header className="App-header">
+						<div onClick={() => {this.handleVotingStuff(1)}}>
+							<ImageContainer>
+								<img src={this.state.URL.A1} className="panelImg"></img>
+								<img src={this.state.URL.B1} className="panelImg"></img>
+								<img src={this.state.URL.C1} className="panelImg"></img>
+							</ImageContainer>
+						</div>
+						Which is funnier?
+						<div onClick={() => {this.handleVotingStuff(2)}}>
+							<ImageContainer>
+								<img src={this.state.URL.A2} className="panelImg"></img>
+								<img src={this.state.URL.B2} className="panelImg"></img>
+								<img src={this.state.URL.C2} className="panelImg"></img>
+							</ImageContainer>
+						</div>
+					</header>
                 </div>
             )
         }
