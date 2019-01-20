321f97003a60dff06e13eb5d2d889d145b5a9f61
Alex Nordstrom <6267244+HyperEpsilon@users.noreply.github.com>
2019-01-20 09:59:01 -0700

    Got setState working with arbitrary files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ImageContainer from './components/ImageContainer';
 import './App.css';
+import firebase from './Firebase.js';
 
 export default class Page extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-			URL: null,
-			id: null	
+			data: null	
         }
     }
 
@@ -17,33 +17,35 @@ export default class Page extends React.Component {
     }
 
     handleVotingStuff(comic_clicked) {
-        if (comic_clicked == 1) {
+        if (comic_clicked === 1) {
             alert("comic 1 clicked");
         } else {
             alert("comic 2 clicked");
 		}
 		
 		this.getImages();
-    }
+	}
 
     getImages() {
-        // getMyDatabaseStuff(query)
-        // .then( (result) => {
 
-            this.setState({URL:{
-                A1: "https://firebasestorage.googleapis.com/v0/b/searchforthebestgarfieldcomic.appspot.com/o/panels%2F1985-01-01a.gif?alt=media&token=8fae9162-f80c-45c1-b151-5a7c4804f7bc",
-                B1: "https://firebasestorage.googleapis.com/v0/b/searchforthebestgarfieldcomic.appspot.com/o/panels%2F1985-01-01b.gif?alt=media&token=7bef96aa-7418-4d87-9ce4-d7fbbbd035a0",
-                C1: "https://firebasestorage.googleapis.com/v0/b/searchforthebestgarfieldcomic.appspot.com/o/panels%2F1985-01-01c.gif?alt=media&token=c53f2fed-101d-4847-95c8-6b442798906e",
-                A2: "https://firebasestorage.googleapis.com/v0/b/searchforthebestgarfieldcomic.appspot.com/o/panels%2F1985-08-05a.gif?alt=media&token=14659a01-a09d-4fdd-b2b6-faee2445eb26",
-                B2: "https://firebasestorage.googleapis.com/v0/b/searchforthebestgarfieldcomic.appspot.com/o/panels%2F1985-08-05b.gif?alt=media&token=f7dc28d8-377d-4a17-90bd-bc052153cf2d",
-                C2: "https://firebasestorage.googleapis.com/v0/b/searchforthebestgarfieldcomic.appspot.com/o/panels%2F1985-08-05c.gif?alt=media&token=c3df0020-75bb-4f7c-8155-5b98cac93274"
-            }})
-        // })
+	// getMyDatabaseStuff(query)
+
+		this.setState({data:{
+			A1: "https://searchforthebestgarfieldcomic.com/panels/1985-01-01a.gif",
+			B1: "https://searchforthebestgarfieldcomic.com/panels/1985-01-01b.gif",
+			C1: "https://searchforthebestgarfieldcomic.com/panels/1985-01-01c.gif",
+			A2: "https://searchforthebestgarfieldcomic.com/panels/1985-01-02a.gif",
+			B2: "https://searchforthebestgarfieldcomic.com/panels/1985-01-02b.gif",
+			C2: "https://searchforthebestgarfieldcomic.com/panels/1985-01-02c.gif"
+		}})
+
+	        
+
     }
 
 
     render() {
-        if(!this.state.URL) {
+        if(!this.state.data) {
             return null;
         } else {
             return (
@@ -51,17 +53,17 @@ export default class Page extends React.Component {
 					<header className="App-header">
 						<div onClick={() => {this.handleVotingStuff(1)}}>
 							<ImageContainer>
-								<img src={this.state.URL.A1} className="panelImg"></img>
-								<img src={this.state.URL.B1} className="panelImg"></img>
-								<img src={this.state.URL.C1} className="panelImg"></img>
+								<img src={this.state.data.A1} className="panelImg"></img>
+								<img src={this.state.data.B1} className="panelImg"></img>
+								<img src={this.state.data.C1} className="panelImg"></img>
 							</ImageContainer>
 						</div>
 						Which is funnier?
 						<div onClick={() => {this.handleVotingStuff(2)}}>
 							<ImageContainer>
-								<img src={this.state.URL.A2} className="panelImg"></img>
-								<img src={this.state.URL.B2} className="panelImg"></img>
-								<img src={this.state.URL.C2} className="panelImg"></img>
+								<img src={this.state.data.A2} className="panelImg"></img>
+								<img src={this.state.data.B2} className="panelImg"></img>
+								<img src={this.state.data.C2} className="panelImg"></img>
 							</ImageContainer>
 						</div>	
 					</header>
