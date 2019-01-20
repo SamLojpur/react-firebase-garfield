f7be10f70b7891b389ea9c527a772fbcf2feb768
ducklin5 <blue-boy-cool@hotmail.com>
2019-01-19 19:30:19 -0700

    web stuff

--- src/App.js
+++ src/App.js
@@ -2,27 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class ComicStrip extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			panelA: "idfsdf",
+			panelB: "",
+			panelC: ""
+		};
+	}
+
+	render() {
+		return (
+			<div className="reconComic">
+				<div> 
+					{panelA} 
+				</div>
+			</div>
+		);
+	}
+}
+
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            I'm sorry Sam, I love you!!
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					Which one is funnier?
+					<ComicStrip />
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
