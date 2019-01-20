47b8bf24a57443803ec202013371e4c0052f7981
Unknown <piplup55@shaw.ca>
2019-01-20 02:30:30 -0700

    APIKEYSLOT

---
+++ src/Firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase/app';
+import 'firebase/database';
+
+//import 'firebase/storage';
+
+let config = {
+    apiKey: "API_KEY",
+    authDomain: "searchforthebestgarfieldcomic.firebaseapp.com",
+    databaseURL: "https://searchforthebestgarfieldcomic.firebaseio.com",
+    projectId: "searchforthebestgarfieldcomic",
+    storageBucket: "searchforthebestgarfieldcomic.appspot.com",
+    messagingSenderId: "913124011844"
+  };
+firebase.initializeApp(config);
+
+export default firebase;
